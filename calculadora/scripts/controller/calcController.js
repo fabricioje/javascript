6b0cd99d65521152415e7a0a0267b0deef68ec07
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2019-04-22 13:01:27 -0300

    Botão igual

--- calculadora/scripts/controller/calcController.js
+++ calculadora/scripts/controller/calcController.js
@@ -2,6 +2,9 @@ class CalcController{
 
     constructor(){
 
+        this._lastOperator = '';
+        this._lastNumber = '';
+
         this._operation = [];
         this._locate = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -21,6 +24,8 @@ class CalcController{
             this.setDisplayDateTime();
 
         }, 1000);
+
+        this.setLastNumberToDisplay();
     }
     
     addEventListenerAll(element, events, fn){
@@ -34,11 +39,15 @@ class CalcController{
     clearAll(){
 
         this._operation = [];
+
+        this.setLastNumberToDisplay();
     }
 
     clearEntry(){
 
         this._operation.pop();
+
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation(){
@@ -51,7 +60,7 @@ class CalcController{
         this._operation[this._operation.length - 1] = value;
     }
 
-    isOperation(value){
+    isOperator(value){
 
         return ['+','-','*', '%', '/'].indexOf(value) > -1;            
     }
@@ -66,29 +75,75 @@ class CalcController{
         }
     }
 
+    getResult(){
+
+        return eval(this._operation.join(""));
+    }
+
     calc(){
 
-        let last = this._operation.pop();
+        let last = '';
+        this._lastOperator = this.getLastItem();
 
-        let result = eval(this._operation.join(""));
+        if (this._operation.length < 3) {
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
+        }
+
+        if (this._operation.length > 3) {
+
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        } else if (this._operation.length == 3) {
+            
+            this._lastNumber = this.getLastItem(false);            
+        }
+
+        let result = this.getResult();
+
+        if (last == '%') {
+            
+            result /= 100;
+
+            this._operation = [result];
+            
+        } else {            
         
-        this._operation = [result, last];
+            this._operation = [result];
+            
+            if(last) this._operation.push(last);
+        }        
 
         this.setLastNumberToDisplay();
+    }
 
+    getLastItem(isOperator = true){
+
+        let lastItem;
+
+        for(let i = this._operation.length-1; i >= 0; i--){
+            
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i];
+                break;
+            }
+        }
+
+        if (!lastItem) {
+            
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+
+        return lastItem;
     }
 
     setLastNumberToDisplay(){
 
-        let lastNumber;
+        let lastNumber = this.getLastItem(false);        
 
-        for(let i = this._operation.length-1; i >= 0; i--){
-
-            if(!this.isOperation(this._operation[i])){
-                lastNumber = this._operation[i];
-                break;
-            }
-        }
+        if (!lastNumber) lastNumber = 0;                    
 
         this.displayCalc = lastNumber;
     }
@@ -97,7 +152,7 @@ class CalcController{
 
         if (isNaN(this.getLastOperation())) {
             
-            if (this.isOperation(value)) {
+            if (this.isOperator(value)) {
                 
                 this.setLastOperation(value);
 
@@ -112,7 +167,7 @@ class CalcController{
             }
         }else{
 
-            if (this.isOperation(value)) {
+            if (this.isOperator(value)) {
                 
                 this.pushOperation(value);
 
@@ -158,7 +213,7 @@ class CalcController{
                 this.addOperation('%');
                 break;
             case 'igual':
-                this.addOperation('+');
+                this.calc();
                 break;
             case 'ponto':
                 this.addOperation('.');

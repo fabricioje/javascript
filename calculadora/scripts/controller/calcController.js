73a6d5f3c59430e88611197fa2af445356f918e5
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2019-04-22 09:07:25 -0300

    Atualizando os números no display

--- calculadora/scripts/controller/calcController.js
+++ calculadora/scripts/controller/calcController.js
@@ -74,11 +74,23 @@ class CalcController{
         
         this._operation = [result, last];
 
+        this.setLastNumberToDisplay();
+
     }
 
     setLastNumberToDisplay(){
 
-        
+        let lastNumber;
+
+        for(let i = this._operation.length-1; i >= 0; i--){
+
+            if(!this.isOperation(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this.displayCalc = lastNumber;
     }
 
     addOperation(value){
@@ -95,6 +107,8 @@ class CalcController{
             }else{
 
                 this.pushOperation(value);
+
+                this.setLastNumberToDisplay();
             }
         }else{
 

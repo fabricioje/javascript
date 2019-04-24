d3d197a3eb88cc2164a0f027860af48da87b25ea
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2019-04-24 09:10:15 -0300

    Implementação do audio ao clicar nos botões

--- calculadora/scripts/controller/calcController.js
+++ calculadora/scripts/controller/calcController.js
@@ -2,6 +2,8 @@ class CalcController{
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -53,11 +55,36 @@ class CalcController{
 
         this.setLastNumberToDisplay();
         this.pastFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+            });
+        })
+    }
+
+    toggleAudio(){
+
+        //this._audioOnOff = (this._audioOnOff) ? false : true;
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio(){
+
+        if (this._audioOnOff) {
+            
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard(){
 
         document.addEventListener('keyup', e=>{
+
+            this.playAudio();
 
             switch (e.key) {
                 case 'Escape':
@@ -274,6 +301,8 @@ class CalcController{
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value) {
             case 'ac':

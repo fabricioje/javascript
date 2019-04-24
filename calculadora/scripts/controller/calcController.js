2538a0d681d3c4d32cbf2c2f5b81d55f6bb52f7d
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2019-04-24 08:39:53 -0300

    Implementação do Crt+C e Crt+V

--- calculadora/scripts/controller/calcController.js
+++ calculadora/scripts/controller/calcController.js
@@ -16,6 +16,31 @@ class CalcController{
         this.initKeyboard();
     }
 
+    pastFromClipboard(){
+
+        document.addEventListener('paste', e=>{
+
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+        });
+    }
+
+    copyToClipboard(){
+
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
+    }
+
     initialize(){
 
         this.setDisplayDateTime();
@@ -27,6 +52,7 @@ class CalcController{
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pastFromClipboard();
     }
 
     initKeyboard(){
@@ -66,6 +92,9 @@ class CalcController{
                 case '8':
                 case '9':
                     this.addOperation(parseInt(e.key));
+                    break;
+                case 'c':
+                    if(e.ctrlKey) this.copyToClipboard();
                     break;
             }
         });

1afa25d6cc7ce009f1f2f476f4c8f1b68ee312da
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2019-07-30 09:24:17 -0300

    Iniciando a implementação dos eventos

---
+++ projeto-usuarios/index.js
@@ -0,0 +1,36 @@
+/*var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputfile");
+var admin = document.querySelector("#exampleInputAdmin");*/
+
+
+
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user ={};
+
+fields.forEach(function(field, index){
+
+    if (field.name == "gender") {
+
+        if (field.checked) {
+            user[field.name] = field.value;
+        }
+        
+    }else{
+        user[field.name] = field.value;
+    }
+});
+
+console.log(user);
+
+document.getElementById("form-user-create").addEventListener("submit", function (event) {
+    
+    event.preventDefault();
+
+    console.log("OK");
+})

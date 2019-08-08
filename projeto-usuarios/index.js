226f2b45ea2fcb1e6355fd628563a02642a324fb
Fabrício J. Silva <fabricio_fx@hotmail.com>
2019-08-07 22:18:03 -0300

    Criado a classe User

--- projeto-usuarios/index.js
+++ projeto-usuarios/index.js
@@ -5,7 +5,7 @@ function addLine(dataUser) {
      
     var tr = document.createElement("tr");
 
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
         <tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -18,11 +18,7 @@ function addLine(dataUser) {
             </td>
         </tr>
     `;
-
-    document.getElementById("table-users").appendChild(tr);
 }
-
-console.log(user);
 
 document.getElementById("form-user-create").addEventListener("submit", function (event) {
     
@@ -41,5 +37,7 @@ document.getElementById("form-user-create").addEventListener("submit", function 
         }
     });
 
-    addLine(user);
+    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+
+    addLine(objectUser);
 })

9e9970cacbb9b5c1019b77d25a1d8d2ca49becdb
Fabrício Silva <fabriciosilva@rvtecnologia.com.br>
2019-09-05 08:42:23 -0300

    Trazendo os dados quando clicar para editar o usuário

--- projeto-usuarios/controllers/UserController.js
+++ projeto-usuarios/controllers/UserController.js
@@ -151,9 +151,21 @@ class UserController{
 
                 if(field){
 
-                    if(field.type == 'file') continue;
-
-                    field.value = json[name];
+                    switch (field.type) {
+                        case 'file':
+                            continue;
+                            break;
+                        case 'radio':
+                                field = form.querySelector("[name=" + name.replace("_","") + "][value=" + json[name] + "]");
+                                field.checked = true;
+                                break;
+                        case 'checkbox':
+                            field.checked = json[name];
+                            break;
+                    
+                        default:
+                            field.value = json[name];
+                    }
                 }
             }
 
